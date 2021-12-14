ae3a1657e95fe3b729296e89be515c96835dd476
Alex Raju <alexraju29@gmail.com>
2021-12-14 16:35:44 +0530

    The problem with the Dot operator is solved

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
+Boolean: isDotClick = false;
+
 function buttonClick(val) {
 
     document.getElementById("display2").value += val;
+}
+
+function operatorClick(val){
+    
+    document.getElementById("display2").value += val;
+    isDotClick=false;
 }
 
 function clearClick() {
 
     document.getElementById("display2").value = "";
     document.getElementById("display1").value = "";
+    isDotClick=false;
 }
+
+function dotClick() {
+
+    
+
+    if (isDotClick == true) {
+
+        
+    } 
+    else {
+        
+         document.getElementById("display2").value += ".";
+         isDotClick = true;
+
+    }
+   
+}
+
 
 function equalClick() {
 
     var text = document.getElementById("display2").value;
-    
+
     document.getElementById("display1").value = text;
-    
+
     var result = eval(text);
-    
-    
+
+
     document.getElementById("display2").value = result;
+    
+    isDotClick=false;
 }
